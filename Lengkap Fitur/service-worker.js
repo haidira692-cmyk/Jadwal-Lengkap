5fc51dea8e7b4f6c88fc9ac472f4ecb64b2f8c61
haidira692-cmyk <haidira692@gmail.com>
2026-02-23 15:50:09 +0800

    Update service-worker.js

--- Lengkap Fitur/service-worker.js
+++ Lengkap Fitur/service-worker.js
@@ -1,10 +1,26 @@
-// Daftarkan Service Worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js')
-    .then(() => console.log("Service Worker Registered"))
-    .catch(err => console.log("Service Worker Failed", err));
-}
+const CACHE_NAME = 'imsakiyah-pro-v5.3';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './jadwal.js',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/fuse.js@6.6.2',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
+];
 
-// Tambahkan link manifest di dalam tag <head>
-// Paste ini di bagian <head> file index.html:
-// <link rel="manifest" href="manifest.json">
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Fetching Assets dari Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
